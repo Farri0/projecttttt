5c898d5a92cd89e2ffcaef3602c6d32aedb3584a
Max <example@gmail.com>
2022-11-27 17:20:34 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 "use strict";
 
-console.log('arr' + ' - object');
+const number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: number0Films,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(--decr);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(5%2);
-console.log(2*4 === '8');
-
-const isChecked = true,
-      isClose = true;
-
-console.log(isChecked && !isClose);
+console.log(personalMovieDB);
