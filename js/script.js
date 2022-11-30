86d9c726907b58178a6ffef03278e6effa5ac651
Max <example@gmail.com>
2022-11-30 17:00:50 +0300

    2practik

--- js/script.js
+++ js/script.js
@@ -10,12 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('да!');
+    } else {
+        console.log('нет!');
+        i--;
+        alert('ответье на вопросы!');
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Мало фильмов посмотрел');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы база');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы прям киноман');
+} else {
+    console.log('ошибка');
+}
+
 
 console.log(personalMovieDB);
+
